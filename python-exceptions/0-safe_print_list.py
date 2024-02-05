4c033ba673e849712f04ddd684259854cde6533f
Diego29012 <dieperey598@gmail.com>
2024-02-04 16:56:12 -0800

    task 0 fixed

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-
 def safe_print_list(my_list=[], x=0):
     try:
         for i in range(x):
@@ -8,4 +7,4 @@ def safe_print_list(my_list=[], x=0):
         return x
     except:
         print("", end="\n")
-        return (i)
+        return i
