ae9f9e21d8764f7b166d4ee32c1ae7aa617d56bf
Diego29012 <dieperey598@gmail.com>
2024-02-06 10:34:34 -0800

    bug fixed

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -3,17 +3,17 @@
 
 class Rectangle:
     """defines a Rectangle class"""
-def __init__(self, width=0, height=0):
-      self.__width = width
-      self.__height = height
+    def __init__(self, width=0, height=0):
+        self.__width = width
+        self.__height = height
 
     @property
     def width(self):
-    	return self.__width   
+        return self.__width   
 
     @width.setter
     def width(self, value):
-	if not isinstance(value, int):
+        if not isinstance(value, int):
             raise TypeError("width must be an integer")
         elif value < 0:
             raise ValueError("width must be >= 0")
@@ -30,3 +30,4 @@ def __init__(self, width=0, height=0):
         elif value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
+
