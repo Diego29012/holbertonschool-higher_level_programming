7156fd8284a0fc046c6568515cd7018c9cc29751
Diego29012 <dieperey598@gmail.com>
2024-02-06 10:30:23 -0800

    task 1 add little things

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -21,8 +21,12 @@ def __init__(self, width=0, height=0):
        
     @property
     def height(self):
-        
+        return self.__height
 
     @height.setter
     def height(self, value):
-        
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
