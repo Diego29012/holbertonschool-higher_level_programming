875bd94156ea53d245f7e9a8645e23b133fb1270
Diego29012 <dieperey598@gmail.com>
2024-02-06 08:57:56 -0800

    task 1

---
+++ python-more_classes/1-rectangle.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Creates a Rectangle class"""
+
+class Rectangle:
+    """defines a Rectangle class"""
+def __init__(self, width=0, height=0):
+      self.__width = width
+      self.__height = height
+
+    @property
+    def width(self):
+       
+
+    @width.setter
+    def width(self, value):
+        
+
+    @property
+    def height(self):
+        
+
+    @height.setter
+    def height(self, value):
+        
