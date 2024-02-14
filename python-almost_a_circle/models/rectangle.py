95c236dd76dc6e1332b5ae067a3de004a52befb4
Diego29012 <dieperey598@gmail.com>
2024-02-13 21:23:37 -0800

    fixed again

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -1,17 +1,24 @@
+#!/usr/bin/python3
+"""comments"""
+
+
+from models.base import Base
+
+
 class Rectangle(Base):
     """comments"""
     def __init__(self, width, height, x=0, y=0, id=None):
         """comments"""
         super().__init__(id)
-        self._width = width
-        self._height = height
-        self._x = x
-        self._y = y
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
 
     @property
     def width(self):
         """comments"""
-        return self._width
+        return self.__width
 
     @width.setter
     def width(self, value):
@@ -21,12 +28,12 @@ class Rectangle(Base):
         elif value <= 0:
             raise ValueError("width must be > 0")
         else:
-            self._width = value
+            self.__width = value
 
     @property
     def height(self):
         """comments"""
-        return self._height
+        return self.__height
 
     @height.setter
     def height(self, value):
@@ -36,12 +43,12 @@ class Rectangle(Base):
         elif value <= 0:
             raise ValueError("height must be > 0")
         else:
-            self._height = value
+            self.__height = value
 
     @property
     def x(self):
         """comments"""
-        return self._x
+        return self.__x
 
     @x.setter
     def x(self, value):
@@ -51,12 +58,12 @@ class Rectangle(Base):
         elif value < 0:
             raise ValueError("x must be >= 0")
         else:
-            self._x = value
+            self.__x = value
 
     @property
     def y(self):
         """comments"""
-        return self._y
+        return self.__y
 
     @y.setter
     def y(self, value):
@@ -66,9 +73,7 @@ class Rectangle(Base):
         elif value < 0:
             raise ValueError("y must be >= 0")
         else:
-            self._y = value
-
+            self.__y = value
     def area(self):
         """Return the area of rectangle, how? width mult height"""
         return self.width * self.height
-
