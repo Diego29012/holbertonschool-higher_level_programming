2ac76bc7ef9241e9709d01b57f947bb8442e1188
Diego29012 <dieperey598@gmail.com>
2024-02-13 20:08:45 -0800

    add comments

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
-
+"""added this line """
 import json 
 
 class Base:
+    """added this line """
     __nb_objects = 0
 
     def __init__(self, id=None):
+        """added this line """
         if id is not None:
             self.id = id
         else:
