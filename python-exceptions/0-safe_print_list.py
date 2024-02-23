c0c7769083c59574789ecdae3ae7d3e61f9c6db4
Diego Piñeyro <124789249+Diego29012@users.noreply.github.com>
2024-02-23 10:51:05 -0300

    Update 0-safe_print_list.py

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     try:
-        for i in range(x):
-            print("{}".format(my_list[i]), end="")
-        print("", end="\n")
-        return x
-    except:
-        print("", end="\n")
-        return i
+        for d in range(x):
+            print(my_list[j], end="")
+    except Exception:
+        print()
+        return d
+    print()
+    return x
