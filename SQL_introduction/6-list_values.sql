9828f10ead70154b9b717ed9edcc69082fb976d2
Diego Piñeyro <124789249+Diego29012@users.noreply.github.com>
2024-03-11 22:15:19 -0300

    add 6-list_values.sql

---
+++ SQL_introduction/6-list_values.sql
@@ -0,0 +1,6 @@
+-- Write a script that lists all rows of the table first_table from the database hbtn_0c_0
+
+-- SELECT: means you want to select all columns from the table.
+-- FROM: This specifies the table from which you want to select the data
+
+SELECT * FROM first_table
