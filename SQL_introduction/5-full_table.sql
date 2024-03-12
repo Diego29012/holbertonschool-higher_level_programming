fa3084f2e410f22be8983db0ba3b366c2d6842b3
Diego Piñeyro <124789249+Diego29012@users.noreply.github.com>
2024-03-11 22:11:33 -0300

    add 5-full_table.sql

---
+++ SQL_introduction/5-full_table.sql
@@ -0,0 +1,3 @@
+--  a script that prints the following description of the table first_table from the database hbtn_0c_0
+-- in this case i use the "show" that provides information about databases, tables, columns, or status is like a "print"
+SHOW CREATE TABLE first_table
