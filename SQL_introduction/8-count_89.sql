df9a04df4fa5dd8f09df385d24ac2d76b069651f
Diego Piñeyro <124789249+Diego29012@users.noreply.github.com>
2024-03-11 23:57:11 -0300

    add 8-count_89.sql

---
+++ SQL_introduction/8-count_89.sql
@@ -0,0 +1,5 @@
+-- a script that creates a table second_table in the database hbtn_0c_0
+-- SELECT COUNT: This part of the query selects the count of rows from the specified table
+-- FROM: This specifies the table from which the count of rows will be calculated.
+-- WHERE: This is a condition that filters the rows
+SELECT COUNT(*) FROM first_table WHERE id = 89;
