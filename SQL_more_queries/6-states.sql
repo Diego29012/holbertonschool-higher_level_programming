77b5b7ea75519a83ef0fd1a8e0d01a22ee1393f2
Diego Piñeyro <124789249+Diego29012@users.noreply.github.com>
2024-03-12 23:26:35 -0300

    add  6-states.sql

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -1,4 +1,9 @@
 -- a script that creates the database hbtn_0d_usa and the table states
 
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states;
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS states 
+  (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(256) NOT NULL
+);
